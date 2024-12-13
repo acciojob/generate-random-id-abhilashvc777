397c760b5050df7b85ebf82d1226ca953dcaa7c4
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2024-12-13 19:57:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function makeid(l) {
   // write your code here
-}
+	let regex = /^[a-zA-Z0-9]$/
+	let id = ""
+	let char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	for(let i=0; i<l; i++){
+		let a = Math.floor(Math.random()*char.length)
+		id = id+char.substring(a,a+1)
+	}
+	return id 
+} 
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
+const l = prompt("Enter a number.");  
 alert(makeid(l));
